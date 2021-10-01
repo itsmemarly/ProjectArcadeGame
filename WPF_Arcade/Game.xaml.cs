ca2938c11af3db9de5a107b92f576e526704a86f
MaxHBO-ICT <50032364+MaxMeeuwis@users.noreply.github.com>
2021-10-01 12:50:13 +0200

    Feedback naar Kaja

--- WPF_Arcade/Game.xaml.cs
+++ WPF_Arcade/Game.xaml.cs
@@ -31,6 +31,8 @@ namespace WPF_Arcade
         public static int worldWith = 30; //width of the gameworld in tiles
         public static int worldHeight = 16; //height of the gameworld in tiles
         public static string worldSeed = "12fsad"; //seed used to generate the world
+        // Wat is de logica achter de value van worldSeed?
+        // Waarom is worldSeed nu ineens 12fsad geworden i.p.v. 1215 zoals het was?
 
         private static int stoneWeight = 60; //number between 0-100 used to determine how much stone to generate versus air. A higher number means more stone
         private Rectangle[,] tileMap = new Rectangle[worldWith, worldHeight];
@@ -38,27 +40,38 @@ namespace WPF_Arcade
         static Random r = new Random();
 
         static string seed = r.Next().ToString();
+        // Waarom is seed niet hetzelfde als worldSeed, ook al is het wel of geen string?
         NoiseMap map1 = new NoiseMap(worldWith, worldHeight, 75, 2, seed);
         NoiseMap map2 = new NoiseMap(worldWith, worldHeight, 25, 3, seed);
-
 
         private void populateTileMap()
         {
             map1.populateMap();
             map2.populateMap();
+            // Waarom heb je 2 NoiseMaps nodig?
+
 
             for (int x = 0; x < tileMap.GetLength(0); x++)
             {
                 for (int y = 0; y < tileMap.GetLength(1); y++)
                 {
                     //if the noise is smaller than the weighted value, add stone, otherwise add nothing
+                    // Waarom zou je steen willen adden als noise < gewegen value?
+
                     //combined noise at position as int
                     int currentNoise = (int)map1.getNoiseAt(x, y) + (int)map2.getNoiseAt(x, y);
+                    // Waarom heb je 2 NoiseMaps nodig?
+                    // Wat is de reden achter de optelsom?
+
                     if (currentNoise  < stoneWeight)
                     {
                         tileMap[x, y] = newTile();
                         BrushConverter bc = new BrushConverter();
-                        Brush brush = (Brush)bc.ConvertFrom("#0000" + (((int)map1.getNoiseAt(x, y) + (int)map2.getNoiseAt(x, y)) + 16).ToString("X"));
+                        Brush brush = (Brush)bc.ConvertFrom(
+                            "#0000" 
+                            + (currentNoise + 16).ToString("X") // Ik heb (int)map1.getNoiseAt(x, y) + (int)map2.getNoiseAt(x, y) veranderd in currentNoise
+                                                                // Waarom "currentNoise + 16"?
+                            );
                         tileMap[x, y].Fill = brush;
                         addRectangleAtPosition(tileMap[x, y], x * tileSize, y * tileSize);
                     }
@@ -83,7 +96,7 @@ namespace WPF_Arcade
                 Tag = "tile",
                 Height = 64, // Kaja, ik zou hier tileSize van maken
                 Width = 64, // Kaja, ik zou hier tileSize van maken
-                Fill = Brushes.White,
+                Fill = Brushes.White, // waarom wit per se?
                 //Stroke = Brushes.Red
             };
 
