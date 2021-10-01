30315579b17b4346cd31b3d4e9feb54ab3521342
hakoenamatata <koen.evers@hotmail.com>
2021-10-01 19:12:56 +0200

    Koen's screens
    - Win Screen
    - Credits
    - Load File
    - Save File
    - Explanation

---
+++ WPF Schermen Koen/SaveFile/Savescreen/Savescreen/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Savescreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveGame1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveGame2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveGame3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
