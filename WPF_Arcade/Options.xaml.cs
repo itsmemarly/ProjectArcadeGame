27b2d4f5a32efc56a71760d6a2a72eb646ce1b6a
MaxHBO-ICT <50032364+MaxMeeuwis@users.noreply.github.com>
2021-10-03 22:31:58 +0200

    Adding Options Window to Project by Maximiliaan

--- WPF_Arcade/Options.xaml.cs
+++ WPF_Arcade/Options.xaml.cs
@@ -20,6 +20,65 @@ namespace WPF_Arcade
         public Options()
         {
             InitializeComponent();
+            Focus();
+        }
+
+        /*
+         * MasterVolumeSlider.Value = MasterVolume
+         * MusicVolumeSlider.Value = MusicVolume
+         * MasterVolumeSlider.Value = MasterVolume
+         */
+
+        private void MasterVolumeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // MasterVolume is 0
+        }
+
+        private void MasterVolumeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // MasterVolume is MasterVolume
+        }
+
+        private void MusicVolumeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // MusicVolume is 0
+        }
+
+        private void MusicVolumeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // MusicVolume is MusicVolume
+        }
+
+        private void EffectVolumeCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // EffectVolume is 0
+        }
+
+        private void EffectVolumeCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // EffectVolume is EffectVolume
+        }
+
+        private void MasterVolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // MasterVolume = MasterVolumeSlider.Value
+        }
+
+        private void MusicVolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // MusicVolume = MusicVolumeSlider.Value
+        }
+
+        private void EffectVolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // EffectVolume = EffectVolumeSlider.Value
+        }
+
+        private void TerugNaarMenu_Clicked(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            Close();
+            menu.Visibility = Visibility.Visible;
         }
     }
 }
