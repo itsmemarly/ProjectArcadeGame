8bd8af3f3229b3fd829d572d7aea98027db04eed
Marleen ( 玛琳) <31918408+itsmemarly@users.noreply.github.com>
2021-10-28 19:24:50 +0200

    Hierna alles van main pullen

--- WPF_Arcade/Highscore.xaml.cs
+++ WPF_Arcade/Highscore.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,9 +19,20 @@ namespace WPF_Arcade
     /// </summary>
     public partial class Highscore : Window
     {
+
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\xampp\htdocs\HBO\digdug\ProjectArcadeGame\arcadeGame.accdb");
         public Highscore()
         {
             InitializeComponent();
+            Bind();
+        }
+        private void Bind()
+        {
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * from Speler1", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
         }
     }
 }
