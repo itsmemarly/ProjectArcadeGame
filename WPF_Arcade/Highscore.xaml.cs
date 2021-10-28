bbf7e345b113644f6e033a29ad95ca12c098ffdb
Marleen ( 玛琳) <31918408+itsmemarly@users.noreply.github.com>
2021-10-28 16:47:40 +0200

    DB Access

--- WPF_Arcade/Highscore.xaml.cs
+++ WPF_Arcade/Highscore.xaml.cs
@@ -21,20 +21,53 @@ namespace WPF_Arcade
     /// </summary>
     public partial class Highscore : Window
     {
+        public string Namen {
+            get{  return this.PlayerName.Text;}
+            set {this.connection();}
+        }
         public Highscore()
         {
             InitializeComponent();
-        
+           
         }
 
-        public string conString = "Data Source=(localdb)/MSSQLLocalDB;Initial Catalog=C:/XAMPP/HTDOCS/HBO/DIGDUG/PROJECTARCADEGAME/WPF_ARCADE/DATABASE1.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public void connection(string namen, int score) 
+        {
+            MySqlConnection conn1 = new MySqlConnection("Data Source=(localdb)/MSSQLLocalDB;Initial Catalog=C:/XAMPP/HTDOCS/HBO/DIGDUG/PROJECTARCADEGAME/WPF_ARCADE/DATABASE1.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            conn1.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT Naam, Score FROM dbo.Game ORDER BY score DESC", conn1);
+            MySqlDataReader reader1;
+            reader1 = cmd.ExecuteReader();
+            reader1.Read();
+           PlayerName.Text = reader1["Naam"].ToString();
+            
+            //PlayerName.Text = reader1["Naam"].ToString();
+            if (reader1.Read())
+            {
+                //PlayerName.Text = "Truus";
+                // PlayerName.Content = reader1["Naam"].ToString();
+               PlayerScore.Text = reader1["Score"].ToString();
+            }
+            else 
+            {
+                MessageBox.Show("Er is geen data gevonden.");
+            }
+            
+            conn1.Close();
 
+            return PlayerName.Text;
+        }
 
         private void TerugNaarMenu(object sender, RoutedEventArgs e)
         {
             Menu menu = new Menu();
             Close();
             menu.Visibility = Visibility.Visible;
+        }
+
+        private void PlayerName_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
         }
     }
 }
