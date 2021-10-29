7d035169d74308a994ff8cb0396cad5d609093fc
Marleen ( 玛琳) <31918408+itsmemarly@users.noreply.github.com>
2021-10-29 10:20:01 +0200

    Show Highscores

--- WPF_Arcade/Highscore.xaml.cs
+++ WPF_Arcade/Highscore.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Office.CustomUI;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.OleDb;
@@ -20,7 +21,6 @@ namespace WPF_Arcade
     public partial class Highscore : Window
     {
 
-        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\xampp\htdocs\HBO\digdug\ProjectArcadeGame\arcadeGame.accdb");
         public Highscore()
         {
             InitializeComponent();
@@ -28,11 +28,33 @@ namespace WPF_Arcade
         }
         private void Bind()
         {
+            //Link to DB
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\reidi\OneDrive\Documents\testdb_old.mdb");
+
+            //Open connection
             con.Open();
-            OleDbDataAdapter da = new OleDbDataAdapter("Select * from Speler1", con);
+
+            //Add new adapter & datatable for P1
+            OleDbDataAdapter da = new OleDbDataAdapter("Select Naam1 AS Naam, Score1 AS Score from Speler1 ORDER BY Score1 DESC", con);
             DataTable dt = new DataTable();
+            //Fill datatable with adapter
             da.Fill(dt);
+
+            //Link to the right datagrid
+            PlayerName.DataContext = dt;
+
+            OleDbDataAdapter da2 = new OleDbDataAdapter("Select Naam2 AS Naam, Score2 AS Score from Speler1 ORDER BY Score2 DESC", con);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            PlayerScore.DataContext = dt2;
             con.Close();
         }
-    }
+
+        private void TerugNaarMenu(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            Close();
+            menu.Visibility = Visibility.Visible;
+        }
+}
 }
--- WPF_Arcade/Save.xaml.cs
+++ WPF_Arcade/Save.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,23 +19,60 @@ namespace WPF_Arcade
     /// </summary>
     public partial class Save : Window
     {
+
+        //Link to DB
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\reidi\OneDrive\Documents\testdb_old.mdb");
+
         public Save()
         {
             InitializeComponent();
+            Bind();
+        }
+
+        private void Bind()
+        {
+           
+          
         }
         private void SaveGame1_Click(object sender, RoutedEventArgs e)
         {
             //SaveFile 1
+
+            //Open connection
+            con.Open();
+
+            //Add new adapter & datatable for P1
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * From Opslaan Where ID=1", con);
+           
+            con.Close();
         }
 
         private void SaveGame2_Click(object sender, RoutedEventArgs e)
         {
             //SaveFile 2
+
+
+            //Open connection
+            con.Open();
+
+            //Add new adapter & datatable for P1
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * From Opslaan Where ID=2", con);
+
+            con.Close();
         }
 
         private void SaveGame3_Click(object sender, RoutedEventArgs e)
         {
             //SaveFile 3
+
+
+            //Open connection
+            con.Open();
+
+            //Add new adapter & datatable for P1
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * From Opslaan Where ID=3", con);
+
+            con.Close();
         }
         private void Menu_Click(object sender, RoutedEventArgs e)
         {
